36211533edc8c7d4c9dec3337dbca5d3dc2766be
huyxx2 <huyng51204@gmail.com>
2025-11-12 20:58:31 +0700

    Merge master into main

---
+++ backend/src/server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoute from "./routes/authRoute.js";
+import userRoute from "./routes/userRoute.js";
+import pool from "./libs/db.js";
+import cookieParser from "cookie-parser";
+import { protectedRoute } from "./middlewares/authMiddleware.js";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+// middleware
+app.use(express.json());
+app.use(cookieParser()); // Lấy dữ liêu từ cookie đã tạo 
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+
+// public routes
+app.use("/api/auth", authRoute);
+
+// private routes
+app.use(protectedRoute);
+app.use("/api/auth", userRoute);
+
+// Connect DB
+
+await pool
+  .getConnection()
+  .then(() => {
+    console.log(`Kết nối db thành công.`);
+    app.listen(PORT, () => {
+      console.log(`Server chạy trên công ${PORT}`);
+    });
+  })
+  .catch(() => console.log(`Kết nối db thành công.`));
